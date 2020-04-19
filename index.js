d0f8a178827ec8e90667540f870d9a0fd49ef7b5
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-04-19 18:59:50 +0530

    Added debounce function

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: '83941f4a',
+            s: searchTerm
+        }
+    });
+
+    console.log(response.data);
+}
+
+const input = document.querySelector('input');
+
+
+
+const onInput = event => {
+     fetchData(event.target.value);    
+};
+input.addEventListener('input', debounce(onInput, 500));
